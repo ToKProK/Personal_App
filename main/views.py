b3db35b3a8fecd9a3e2988d7258c85dc4b42ccb0
T <T@DESKTOP-26DHSMH>
2025-05-05 18:08:17 +0300

    Сделал простую загрузку файлов без привязки к модели(БД)

--- main/views.py
+++ main/views.py
@@ -1,10 +1,25 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render 
-
+from main.forms import UploadFileForm
 # Create your views here.
 def index(request):
     return render(request, 'main/home.html')
+
+
+
 @login_required #Запрещает заходит не зарегестрированным пользователям
 def about(request):
-    return HttpResponse('About Page')
+    if request.method == 'POST': # Если: получаем POST запрос / загружается изображение
+        #handle_uploaded_file(request.FILES['file_upload']) # "file_upload" Такое название присвоенной в html странице
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(form.cleaned_data['file'])# "file" название атрибута класса "UploadFileForm"
+    else:
+        form = UploadFileForm()
+    return render(request, 'main/about.html', {'form' : form,})
+
+def handle_uploaded_file(f): # 48
+    with open(f"main/images_about/{f.name}", "wb+") as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
